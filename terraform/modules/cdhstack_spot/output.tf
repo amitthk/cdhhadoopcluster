3d64c2c97f65ac63fbca21f2336108685e9c1629
amitthk <e.amitthakur@gmail.com>
2019-03-03 18:54:49 +0800

    update outputs and inventory

--- terraform/modules/cdhstack_spot/output.tf
+++ terraform/modules/cdhstack_spot/output.tf
@@ -1,3 +1,7 @@
+output "spot_cdh_scm" {
+  value = "${aws_spot_instance_request.spot_cdh_scm.*.public_ip}"
+}
+
 output "spot_cdh_master" {
   value = "${aws_spot_instance_request.spot_cdh_master.*.public_ip}"
 }
